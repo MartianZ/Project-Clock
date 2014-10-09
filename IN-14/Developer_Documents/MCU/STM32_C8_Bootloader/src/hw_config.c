06ba100f542b17e5cd97a488b259f2692d216051
MartianZ <fzyadmin@gmail.com>
2014-10-09 22:35:09 +0800

    set DFU mode nofitication
    when enter DFU mode, turn on the on board LED and disable the 5050 RGB
    LED & NixieTube.

--- IN-14/Developer_Documents/MCU/STM32_C8_Bootloader/src/hw_config.c
+++ IN-14/Developer_Documents/MCU/STM32_C8_Bootloader/src/hw_config.c
@@ -254,11 +254,26 @@ void USB_Cable_Config (FunctionalState NewState)
 void DFU_Button_Config(void)
 {  
     GPIO_InitTypeDef GPIO_InitStructure;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB|RCC_APB2Periph_GPIOC, ENABLE);
     GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
     GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOC, &GPIO_InitStructure);
+    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0 | GPIO_Pin_8;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+    
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5 | GPIO_Pin_8 | GPIO_Pin_9;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
+    
+    GPIO_SetBits(GPIOA, GPIO_Pin_0);
+    GPIO_ResetBits(GPIOA, GPIO_Pin_8);
+    GPIO_ResetBits(GPIOB, GPIO_Pin_9 | GPIO_Pin_8 | GPIO_Pin_5);
+    
     
 
 }
