c21b071dd29051e8104add09c19c218c14c5f22c
MartianZ <fzyadmin@gmail.com>
2014-10-13 20:39:50 +0800

    use qt designer to design form

--- IN-14/User_Application/App/nixietube.py
+++ IN-14/User_Application/App/nixietube.py
@@ -1,79 +1,72 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import usb
 import sys
-from PyQt4 import QtGui, QtCore
+import hid
+from PyQt4 import QtGui, QtCore, uic
+import time
 
+form_class = uic.loadUiType("nixietube.ui")[0]
 
-class NixieTubeMainWindow(QtGui.QMainWindow):
+class NixieTubeMainWindow(QtGui.QMainWindow, form_class):
     def __init__(self):
         super(NixieTubeMainWindow, self).__init__()
-        
-        self.init_ui()
-        
-    def init_ui(self):
-        
-        
-        #label
-        self.lbl_device_status = QtGui.QLabel(self)
-        self.lbl_device_status.setText("Device: <b>Unconnected</b>") #Normal, DFU, Unconnected
-        self.lbl_device_status.move(15, 12)
-        self.lbl_device_status.adjustSize()
+        self.setupUi(self)
 
-        
-        
-        #btn
-        #refresh_btn = QtGui.QPushButton('Refresh', self)
-        #refresh_btn.setToolTip('Refresh device connection state.')
-        #refresh_btn.resize(refresh_btn.sizeHint())
-        #refresh_btn.move(200, 6)
-        #refresh_btn.clicked.connect(self.refresh_btn_clicked)
-        
-        #tab
-        tab_widget = QtGui.QTabWidget(self)     # add tab
-        tab1 = QtGui.QWidget()
-        tab2 = QtGui.QWidget()
-        tab_widget.addTab(tab1, "Configuration") 
-        tab_widget.addTab(tab2, "Device Firmware Upgrade")
-        tab_widget.setGeometry(10, 100, 480, 190)
-        
-        
-        #timer
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.timer_one)
         self.timer.start(300)
-        
-        self.setGeometry(0, 0, 500, 300)
-        self.setWindowTitle('NixieTube Assistant')
-        self.setWindowIcon(QtGui.QIcon('icon.png'))        
-        frameGm = self.frameGeometry()
-        screen = QtGui.QApplication.desktop().screenNumber(QtGui.QApplication.desktop().cursor().pos())
-        centerPoint = QtGui.QApplication.desktop().screenGeometry(screen).center()
-        frameGm.moveCenter(centerPoint)
-        self.move(frameGm.topLeft())
+        self.btn_ctime.clicked.connect(self.btn_ctime_clicked)
+        self.connection_status = 0
+
         self.show()
+
+
     def timer_one(self):
         sender = self.sender()
-        
-        busses = usb.busses()
-        for bus in busses:
-            devices = bus.devices
-            for dev in devices:
-                if (dev.idVendor == 1) and (dev.idProduct == 57105): #0xdf11
-                    self.lbl_device_status.setText("Device: <font color=red><b>Connceted[DFU]</b></font>")
-                    self.lbl_device_status.adjustSize()
-                    return
-                if (dev.idVendor == 1203) and (dev.idProduct == 17748):
-                    self.lbl_device_status.setText("Device: <font color=green><b>Connceted[Normal]</b></font>")
-                    self.lbl_device_status.adjustSize()
-                    dev.open()
-                    dev.write("p")
-                    return
-            self.lbl_device_status.setText("Device: <b>Unconnected</b>")
+        d = hid.enumerate(0x04b3, 0x1234)
+
+        if len(d) > 0:
+            self.lbl_device_status.setText("Device: <font color=green><b>Connceted[Normal]</b></font>")
             self.lbl_device_status.adjustSize()
+            self.connection_status = 1
+            return
+        #todo:dfu
+        #if len(d) > 0:
+        #    self.lbl_device_status.setText("Device: <font color=green><b>Connceted[Normal]</b></font>")
+        #    self.lbl_device_status.adjustSize()
+        #    return
+        self.lbl_device_status.setText("Device: <b>Unconnected</b>")
+        self.lbl_device_status.adjustSize()
+        self.connection_status = 0
             
-            
-        
+    def btn_ctime_clicked(self):
+        self.btn_ctime.setText("Sending instructions...")
+        if self.connection_status == 1:
+            try:
+                h = hid.device()
+                h.open(0x04b3, 0x1234)
+                s = time.strftime("cs%Scm%Mch%Hcy%yco%mcd%d",time.localtime(time.time()))
+                print s
+                x = []
+                for c in s:
+                    x.append(ord(c))
+                h.write(x)
+                while 1:
+                    data = h.read(64, 3000)
+                    if data:
+                        sys.stdout.write(''.join(chr(i) for i in data))
+                        sys.stdout.flush()
+                    else:
+                        break
+                h.close()
+                
+                QtGui.QMessageBox.information(self, 'Message', "Successfully calibrate NixieTube clock time!", QtGui.QMessageBox.Yes, QtGui.QMessageBox.Yes)
+            except ex:
+                print ex
+            finally:
+                self.btn_ctime.setText("Calibrate NixieTube Clock Time")
+
+
 def main():
     
     app = QtGui.QApplication(sys.argv)
