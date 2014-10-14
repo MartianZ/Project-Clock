27f834d3cc3dc07cac4a8b56344ee932215156c7
MartianZ <fzyadmin@gmail.com>
2014-10-14 16:58:28 +0800

    support detect dfu device

--- IN-14/User_Application/App/nixietube.py
+++ IN-14/User_Application/App/nixietube.py
@@ -4,6 +4,10 @@ import sys
 import hid
 from PyQt4 import QtGui, QtCore, uic
 import time
+import usb
+from dfuse import *
+from intel_hex import *
+
 
 form_class = uic.loadUiType("nixietube.ui")[0]
 
@@ -27,6 +31,26 @@ class NixieTubeMainWindow(QtGui.QMainWindow, form_class):
         self.show()
 
 
+    def get_dfu_device(self):
+        busses = usb.busses()
+        for bus in busses:
+            devices = bus.devices
+            for dev in devices:
+                if dev.idVendor != 0x0001:
+                    continue
+                if dev.idProduct != 0xdf11:
+                    continue
+                for config in dev.configurations:
+                    for intf in config.interfaces:
+                        for alt in intf:
+                            if alt.interfaceClass == DFU_CLASS and \
+                                    alt.interfaceSubClass == DFU_SUBCLASS and \
+                                    (alt.interfaceProtocol == DFU_STM32PROTOCOL_0 or \
+                                         alt.interfaceProtocol == DFU_STM32PROTOCOL_2):
+                                return dev, config, alt
+        raise ValueError, "Device not found"
+
+
     def timer_one(self):
         sender = self.sender()
         d = hid.enumerate(0x04b3, 0x1234)
@@ -39,11 +63,16 @@ class NixieTubeMainWindow(QtGui.QMainWindow, form_class):
             self.tab1.setEnabled(True)
             self.tab2.setEnabled(False)
             return
-        #todo:dfu
-        #if len(d) > 0:
-        #    self.lbl_device_status.setText("Device: <font color=green><b>Connceted[Normal]</b></font>")
-        #    self.lbl_device_status.adjustSize()
-        #    return
+
+        dev = usb.core.find(idVendor=0x0001, idProduct=0xdf11)
+        if dev is not None:
+            self.lbl_device_status.setText("Device: <font color=red><b>Connceted[DFU]</b></font>")
+            self.lbl_device_status.adjustSize()
+            self.connection_status = 2
+            self.tab1.setEnabled(False)
+            self.tab2.setEnabled(True)
+            return
+
         self.lbl_device_status.setText("Device: <b>Unconnected</b>")
         self.lbl_device_status.adjustSize()
         self.connection_status = 0
