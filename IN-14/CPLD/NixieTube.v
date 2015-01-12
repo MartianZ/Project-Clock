aadb9f21a5ba7ad02eb3a9aad5c51a1ff24ede32
Martian Z <fzyadmin@gmail.com>
2015-01-12 20:44:23 +0800

    generate neon pwm wave

--- IN-14/CPLD/NixieTube.v
+++ IN-14/CPLD/NixieTube.v
@@ -1,12 +1,17 @@
-module NixieTube(clk, data, pwm, sel, out, en);
+module NixieTube(clk, data, pwm, sel, out, en, clk_32k, neon_pwm);
 input clk;
 input data;
 input pwm;
 input sel;
 input en;
+input clk_32k;
+
+output neon_pwm;
 output [71:0]out;
 reg [71:0]data1;
 reg [71:0]data2;
+reg [1:0]counter;
+
 
 always@ (posedge clk)
 begin
@@ -23,5 +28,13 @@ end
 assign out = (en == 1) ? 
 			(pwm == 1) ? data1 : data2
 			: 72'b0;
+			
+always@ (posedge clk_32k)
+begin
+	counter = counter + 1;
+end
+
+assign neon_pwm = counter[1];
+
 endmodule
 								
